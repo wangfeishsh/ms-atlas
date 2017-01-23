ea7c4248619d67f63f212b526cbaf2aa7383f0f5
baochunyu <Aa10051544>
2017-01-23 18:06:40 +0800

    init

--- src/main/java/com/bao/MsAtlasApplication.java
+++ src/main/java/com/bao/MsAtlasApplication.java
@@ -3,10 +3,23 @@ package com.bao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.metrics.atlas.EnableAtlas;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableAtlas
 @SpringBootApplication
+@RestController
 public class MsAtlasApplication {
+
+	@GetMapping("/test")
+	public String test(){
+		return "test";
+	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "hello";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsAtlasApplication.class, args);
